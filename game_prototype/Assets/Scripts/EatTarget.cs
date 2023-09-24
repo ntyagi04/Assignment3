9b8a4736df01c217b69e0a6a50a88dab8ff39862
Kaiyuan Wang <kaiyuanwang2089@gmail.com>
2023-09-23 21:50:48 -0700

    meet target mach

---
+++ game_prototype/Assets/Scripts/EatTarget.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //if collision between player and electron happened
+            //make electron merge into player and player change into larger size
+
+        if (other.name == "player"){
+
+            Destroy(this.gameObject);
+            other.gameObject.transform.localScale += new Vector3(1,1,1);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
